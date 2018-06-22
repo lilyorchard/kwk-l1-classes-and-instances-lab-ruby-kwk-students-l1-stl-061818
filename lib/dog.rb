f06abd71587d3a4954f602fe5e0ba5c3465a2b4b
lilyorchard <lily@orchardfamily.com>
2018-06-22 17:21:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# dog.rb
+class Dog
+  def initialize(loyal,cute,breed)
+    @loyal = loyal
+    @cute = cute
+    @breed = breed
+  end
+  
+  def is_it_loyal
+    @loyal
+  end
+end
+fido = Dog.new("He is super loyal","pretty cute","half pointer")
+snoopy = Dog.new("pretty","very","beagle")
+lassie = Dog.new("very","yeah","long-haired Collie")
+puts "Is fido loyal? #{fido.is_it_loyal}."
