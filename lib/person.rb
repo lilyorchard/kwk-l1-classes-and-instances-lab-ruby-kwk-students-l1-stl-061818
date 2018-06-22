1aa60c408704cd17872b951c9f6d8519670b92b5
lilyorchard <lily@orchardfamily.com>
2018-06-22 17:21:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
-# person.rb
+class Person
+  def initialize(gender,age)
+    @gender = gender
+    @age = age
+  end 
+  
+  def what_gender
+    @gender
+  end
+end
+adele_goldberg = Person.new("female","72")
+alan_kay = Person.new("male","78")
+puts "What is adele's gender? She is a #{adele_goldberg.what_gender}."
